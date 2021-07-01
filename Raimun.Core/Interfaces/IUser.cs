be723ad0ec29a044bed75d230efa45920c3762fa
mozhganzeyghami <80096658+mozhganzeyghami@users.noreply.github.com>
2021-07-01 21:53:20 +0430

    Add files via upload
    files2

---
+++ Raimun.Core/Interfaces/IUser.cs
@@ -0,0 +1,14 @@
+﻿using Raimun.DataAccessLayer.Entities;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Raimun.Core.Interfaces
+{
+   public interface IUser
+   {
+      Task<User> InsertUser(User user);
+      bool LoginUser(string UserName, string password);
+      List<User> GetAllUsers();
+     
+   }
+}
---
+++ Raimun.Core/Interfaces/IWeather.cs
@@ -0,0 +1,12 @@
+﻿using Raimun.DataAccessLayer.Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Raimun.Core.Interfaces
+{
+   public interface IWeather
+   {
+      void InsertWeatherInfo(Weather weather);
+   }
+}
